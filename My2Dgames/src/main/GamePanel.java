08f93052bc8fee8e8e5b9ef5bfb1d458a557bbf6
Hidro.nthn <minhthuan24062004@gmail.com>
2023-12-11 22:46:09 +0700

    Modifications to Binh's upload

--- My2Dgames/src/main/GamePanel.java
+++ My2Dgames/src/main/GamePanel.java
@@ -11,20 +11,20 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JPanel;
 
-public class GamePanel extends JPanel implements Runnable {// Jpanel is the subclass of Game panel
+public class GamePanel extends JPanel implements Runnable {     // JPanel is the subclass of Game panel
     // GAME SETTING
-    final int originalTileSize= 16;//16x16 tile standard for 2d game
+    final int originalTileSize= 16;     //16x16 tile standard for 2d game
 
     final int scale=3;
-    public final int tileSize =originalTileSize*scale;// 48x48 pixels on the screen
+    public final int tileSize =originalTileSize*scale;      // 48x48 pixels on the screen
     public final int maxScreenCol=16;
     public final int maxScreenRow=12;
-    public final int screenWidth = tileSize*maxScreenCol;// 768 pixels
-    public final int screenHeight = tileSize*maxScreenRow;//576 pixels
+    public final int screenWidth = tileSize*maxScreenCol;   // 768 pixels
+    public final int screenHeight = tileSize*maxScreenRow;  //576 pixels
 
     //WORLD MAP SETTING
-    public final int maxWorldCol = 40;// this is the width of the map width, if we make the map column is 40 like my prototype, its 40
-    public final int maxWorldRow = 40;// so as here
+    public final int maxWorldCol = 40; // Map width (equals to the X coordinate of the matrix in world01.txt)
+    public final int maxWorldRow = 40; // Map height (equals to the Y coordinate of the matrix in world01.txt)
 
     public final int worldWidth = tileSize * maxScreenCol;
     public final int worldHeight = tileSize * maxScreenRow;
@@ -41,15 +41,14 @@ public class GamePanel extends JPanel implements Runnable {// Jpanel is the subc
     int playerX =100;
     int playerY = 100;
     int playerSpeed =4;
-
     public GamePanel(){
-        this.setPreferredSize(new Dimension(screenWidth,screenHeight));// set the size for the class (Jpanel)
+        this.setPreferredSize(new Dimension(screenWidth,screenHeight)); // set the size for the class (JPanel)
         this.setBackground(Color.black);
-        this.setDoubleBuffered(true);// if set to true, all the drawing from this component will be done in an offscreen Painting buffer
+        this.setDoubleBuffered(true); // if set to true, all the drawing from this component will be done in an Offscreen Painting buffer
         this.addKeyListener(keyH);
-        this.setFocusable(true);// with this, this game panel can be " focused" to recieve key input
-
+        this.setFocusable(true); // with this, this Game Panel can be "focused" to receive key input
     }
+
     public void startGameThread(){
         gameThread = new Thread(this);// passing game panel to this thread constructor
         gameThread.start();
@@ -59,7 +58,7 @@ public class GamePanel extends JPanel implements Runnable {// Jpanel is the subc
     @Override
     public void run() {
 
-        double drawInterval=1000000000/FPS;// this means we draw the screen every 0.016 seconds so we can draw the screen 60 times per second
+        double drawInterval=1000000000/FPS;     // this means we draw the screen every 0.016 seconds so we can draw the screen 60 times per second
         double nextDrawTime = System.nanoTime() + drawInterval; // the next drawtime will be plus this drawInterval
 
         while(gameThread!=null){
@@ -79,9 +78,10 @@ public class GamePanel extends JPanel implements Runnable {// Jpanel is the subc
                     remainingTime=0;
                 }
                 //  this thread doesn't need to sleep since we already used to allocated time
-                Thread.sleep((long) remainingTime);// long only accept mili second
+                Thread.sleep((long) remainingTime);     // long only accept mili second
 
-                nextDrawTime += drawInterval;// sau khi sau 1 chu kỳ nó sẽ tự lặp lại 1 cái drawtime mới
+                nextDrawTime += drawInterval;           // A new "drawtime" will be repetitied after an interval
+
 
             } catch (InterruptedException ex) {
                 Logger.getLogger(GamePanel.class.getName()).log(Level.SEVERE, null, ex);
@@ -94,8 +94,8 @@ public class GamePanel extends JPanel implements Runnable {// Jpanel is the subc
 
     @Override
     public void paintComponent(Graphics g){
-        super.paintComponent(g);// GamePanel means the subclass of Jpanel
-        Graphics2D g2 =(Graphics2D)g;//Graphics2D class extends the Graphics class to provide more sophisticated control over geometry...
+        super.paintComponent(g);        // GamePanel means the subclass of JPanel
+        Graphics2D g2 =(Graphics2D)g;   //Graphics2D class extends the Graphics class to provide more sophisticated control over geometry...
         // this means we changes this graphics g to this Graphics2D class
         tileM.draw(g2);
         player.draw(g2);
