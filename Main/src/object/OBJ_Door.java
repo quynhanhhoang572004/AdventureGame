ba60f53580906787be4323d48b69b467ead21f93
Hidro.nthn <minhthuan24062004@gmail.com>
2023-12-12 11:56:26 +0700

    Refactoring the file structure

---
+++ Main/src/object/OBJ_Door.java
@@ -0,0 +1,17 @@
+package object;
+
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
+public class OBJ_Door extends SuperObject {
+    public OBJ_Door () {
+        name = "Door";
+        try {
+            image = ImageIO.read(new File("res/objects/door.png")); // Using getResourceAsStream when running code will get error
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        collision = true;   // The player cannot go though this object
+    }
+}
