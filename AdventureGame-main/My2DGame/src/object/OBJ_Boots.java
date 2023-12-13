9df22fa2500e15d62dd6697c645cc201ffb5ff5f
lilmonkeygoturback <quantruong0307@gmail.com>
2023-12-13 12:46:44 +0700

    Objects scaling.

--- AdventureGame-main/My2DGame/src/object/OBJ_Boots.java
+++ AdventureGame-main/My2DGame/src/object/OBJ_Boots.java
@@ -4,15 +4,26 @@ import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 
+import main.GamePanel;
+import main.UtilityTool;
+
 public class OBJ_Boots extends SuperObject {
-	public OBJ_Boots () {
-		name = "Boots";
-	        try {
-	            image = ImageIO.read(new File("res/objects/socks.png")); 
-	            // getResourceAsStream is an old structure so it may cause errors
-	        } catch (IOException e) {									 
-	            e.printStackTrace();
-	        }
-	}
+    GamePanel gp;
+    UtilityTool uTool; 
+
+    public OBJ_Boots (GamePanel gp) {
+        this.gp = gp;
+        this.uTool = new UtilityTool(); 
+        name = "Boots";
+        try {
+            image = ImageIO.read(new File("res/objects/boots.png")); 
+            uTool.scaleImage(image, gp.tileSize, gp.tileSize);
+            // getResourceAsStream is an old structure so it may cause errors
+        } catch (IOException e) {                                     
+            e.printStackTrace();
+        }
+    }
+
 }
+
 
--- AdventureGame-main/My2DGame/src/object/SuperObject.java
+++ AdventureGame-main/My2DGame/src/object/SuperObject.java
@@ -5,6 +5,7 @@ import java.awt.Graphics2D;
 import java.awt.Rectangle;
 
 import main.GamePanel;
+import main.UtilityTool;
 
 public class SuperObject {
     public BufferedImage image;
@@ -14,6 +15,8 @@ public class SuperObject {
     public Rectangle solidArea = new Rectangle(0, 0, 48, 48); // The area around the indicated object
     public int solidAreaDefaultX = 0;
     public int solidAreaDefaultY = 0;
+    UtilityTool uTool = new UtilityTool();
+    
 
     public void draw (Graphics2D g2, GamePanel gp) {
         int screenX = worldX - gp.player.worldX + gp.player.screenX;
