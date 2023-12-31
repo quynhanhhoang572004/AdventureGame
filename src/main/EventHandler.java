528e3fa81f59f937a0ca56586d82be04a32cbb26
binhbb2204 <binhbb2204@gmail.com>
2023-12-31 15:20:50 +0700

    make the event won't happen repeatly

--- src/main/EventHandler.java
+++ src/main/EventHandler.java
@@ -4,6 +4,12 @@ package main;
 public class EventHandler {
 	GamePanel gp;
 	EventRect eventRect[][];
+
+	int previousEventX, previousEventY;
+	boolean canTouchEvent = true;
+
+
+
 //using this type of [][] makes the code better to design?:D
 	public EventHandler (GamePanel gp) {
 		this.gp = gp;
@@ -28,17 +34,29 @@ public class EventHandler {
 		
 	}
 	
+	
 	public void checkEvent () {		// This function uses data from the worldV2.txt, and is a sample, can modify later 
-		if (hit(28,21,"right") == true) {
-			damagePit(gp.dialogueState);
-		}//CONCERNING: REMEMBER TO REDUCE THE COORDINATE BY 1 DUE TO THE FACT THAT THIS IS JAVA :V
+		//checl whether the player character is more than 1 tile away from the last event
+		int xDistance = Math.abs(gp.player.worldX - previousEventX);
+		int yDistance = Math.abs(gp.player.worldY - previousEventY);
+		int distance = Math.max(xDistance, yDistance);
+		if(distance > gp.tileSize){
+			canTouchEvent = true;
+		}
+
+		if(canTouchEvent == true){//CONCERNING: REMEMBER TO REDUCE THE COORDINATE BY 1 DUE TO THE FACT THAT THIS IS JAVA :V
+			if (hit(28,21,"any") == true) {
+				damagePit(28, 21, gp.dialogueState);
+			}
 			 // (x,y): Coordinate corresponds to the coordinate in worldV2.txt
-		if (hit(36 ,15,"any") == true) {
-			healingPool(gp.dialogueState);
+			if (hit(36 ,15,"any") == true) {
+				healingPool(36, 15, gp.dialogueState);
+			}
+			if(hit(29, 22, "any") == true){
+				teleport(29, 12, gp.dialogueState);
+			}
 		}
-		if(hit(29, 22, "any") == true){
-			teleport(gp.dialogueState);
-		}
+
 		
 				
 	}
@@ -50,9 +68,12 @@ public class EventHandler {
 		eventRect[col][row].x = col * gp.tileSize + eventRect[col][row].x;
 		eventRect[col][row].y = row * gp.tileSize + eventRect[col][row].y;
 		
-		if(gp.player.solidArea.intersects(eventRect[col][row])) {
+		if(gp.player.solidArea.intersects(eventRect[col][row]) && eventRect[col][row].eventDone == false) {
 			if(gp.player.direction.contentEquals(reqDirection) || reqDirection.contentEquals("any")) {
 				hit = true;
+
+				previousEventX = gp.player.worldX;
+				previousEventY = gp.player.worldY;
 			}
 		}
 		
@@ -64,20 +85,22 @@ public class EventHandler {
 		return hit;		
 	}
 
-	public void teleport(int gameState){
+	public void teleport(int col, int row, int gameState){
 		gp.gameState = gameState;
 		gp.ui.currentDialogue = "Teleport piu piu piu";
 		gp.player.worldX = gp.tileSize*37;
 		gp.player.worldY = gp.tileSize*30;
 	}
 	
-	public void damagePit(int gameState) {
+	public void damagePit(int col, int row, int gameState) {
 		gp.gameState = gameState;
 		gp.ui.currentDialogue = "You fall into a pit!";
 		gp.player.life -= 1;
+		//eventRect[col][row].eventDone = true; //we do this in order to make the event happen once only
+		canTouchEvent = false;
 	}
 	
-	public void healingPool(int gameState) {
+	public void healingPool(int col, int row, int gameState) {
 		if(gp.keyH.enterPressed == true) {
 			gp.gameState = gameState;
 			gp.ui.currentDialogue = "You drink the water. \n Your life has been recovered.";
