e828f8cd07e85f942774528bfe9c833b18b99fa9
binhbb2204 <binhbb2204@gmail.com>
2023-12-31 16:28:01 +0700

    testing, don't mind it

--- src/main/AssetSetter.java
+++ src/main/AssetSetter.java
@@ -12,8 +12,8 @@ public class AssetSetter {
 
     public void setObject () {
         gp.obj[0] = new OBJ_Door(gp);
-        gp.obj[0].worldX = gp.tileSize*21;
-        gp.obj[0].worldY = gp.tileSize*22;
+        gp.obj[0].worldX = gp.tileSize*26;
+        gp.obj[0].worldY = gp.tileSize*26;
     }
     
     public void setNPC () {
