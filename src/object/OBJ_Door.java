f2613d10acacd9b00a678dc975ed2ea1a4c2b378
HoaiBao259 <lehoaibaolhb2004@gmail.com>
2024-01-16 21:02:58 +0700

    add setDialogue and change interact

--- src/object/OBJ_Door.java
+++ src/object/OBJ_Door.java
@@ -19,10 +19,14 @@ public class OBJ_Door extends Entity {
         solidArea.height = 32;
         solidAreaDefaultX = solidArea.x;
         solidAreaDefaultY = solidArea.y;
+
+        setDialogue();
+    }
+    public void setDialogue(){
+        dialogues[0][0]= "You need a key to open this.";
     }
     public void interact(){
-       gp.gameState = gp.dialogueState;
-       gp.ui.currentDialogue="Key is required";
+       startDialogue(this,0);
     }
 }
 
